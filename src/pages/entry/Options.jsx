adbcc7dd7011c0a5e4901009b6dce745096a131a
annpermi <perminova.anna85@gmail.com>
2021-10-29 23:20:29 -0400

    Options test

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
+import Row from "react-bootstrap/Row";
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+  //optionType is 'scoop'or 'toppings'
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, [optionType]);
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+}
---
+++ src/pages/entry/test/Options.test.jsx
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+
+import Options from "../Options";
+
+test("Displays image for each scoop from the server", async () => {
+  render(<Options optionType="scoops" />);
+
+  //find the images
+  //$ - indicates that its at the end of the string, i - case sensitive
+  //when you get async elements appearing in the DOM, you need await and find them
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
+
+  //confirm alt text of images
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("Display image for each topping from the server", async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  const imageTitles = toppingImages.map((el) => el.alt);
+  expect(imageTitles).toEqual([
+    "M&Ms topping",
+    "Cherries topping",
+    "Hot fudge topping",
+  ]);
+});
